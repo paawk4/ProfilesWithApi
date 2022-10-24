06085155c6f84a34cbecb8b43654e1f26a9d8211
unknown <@ngknn.local>
2022-10-24 18:16:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/profileswithapi/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.profileswithapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ProfileAdapter profileAdapter;
+    private List<Profile> listProfiles = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lvProfiles = findViewById(R.id.lvDatabase);
+        profileAdapter = new ProfileAdapter((Context) MainActivity.this, (ArrayList<Profile>) listProfiles);
+        lvProfiles.setAdapter(profileAdapter);
+
+        new GetProfiles().execute();
+    }
+    private class GetProfiles extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("http://ssfb.ngknn.local/NGKNN/%D0%A7%D0%B5%D1%82%D0%B2%D0%B5%D1%80%D0%B8%D0%BA%D0%BE%D0%B2%D0%9F%D0%B2/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String line = "";
+
+                while ((line = reader.readLine()) != null){
+                    result.append(line);
+                }
+                return result.toString();
+            } catch (Exception ex){
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONArray tempArray = new JSONArray(s);
+                for (int i = 0; i < tempArray.length(); i++){
+                    JSONObject profileJson = tempArray.getJSONObject(i);
+                    Profile tempProfile = new Profile(
+                            profileJson.getInt("Id"),
+                            profileJson.getString("Name"),
+                            profileJson.getString("Job"),
+                            profileJson.getString("Image")
+                    );
+                }
+            } catch (Exception ex){
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/profileswithapi/ProfileAdapter.java
@@ -0,0 +1,113 @@
+package com.example.profileswithapi;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ProfileAdapter extends BaseAdapter implements Filterable {
+    LayoutInflater inflater;
+    private ArrayList<Profile> mOriginalValues;
+    private ArrayList<Profile> mDisplayedValues;
+
+    public ProfileAdapter(Context context, ArrayList<Profile> profiles) {
+        this.mOriginalValues = profiles;
+        this.mDisplayedValues = profiles;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mDisplayedValues.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    private static class ViewHolder {
+        RelativeLayout rlContainer;
+        TextView tvName, tvJob, tvEmail;
+        ImageView ivAvatar;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.sample_item, null);
+            holder.rlContainer = convertView.findViewById(R.id.rlContainer);
+            holder.tvName = convertView.findViewById(R.id.Name);
+            holder.tvJob = convertView.findViewById(R.id.Job);
+            holder.tvEmail = convertView.findViewById(R.id.Email);
+            holder.ivAvatar = convertView.findViewById(R.id.list_itemAvatar);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.tvName.setText(mDisplayedValues.get(i).name);
+        holder.tvJob.setText(mDisplayedValues.get(i).job);
+        holder.tvEmail.setText(mDisplayedValues.get(i).email);
+        holder.ivAvatar.setImageBitmap(mDisplayedValues.get(i).image);
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                mDisplayedValues = (ArrayList<Profile>) results.values;
+                notifyDataSetChanged();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<Profile> FilteredArrList = new ArrayList<>();
+
+                if (mOriginalValues == null) {
+                    mOriginalValues = new ArrayList<>(mDisplayedValues);
+                }
+
+                if (constraint == null || constraint.length() == 0) {
+
+                    // set the Original result to return
+                    results.count = mOriginalValues.size();
+                    results.values = mOriginalValues;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < mOriginalValues.size(); i++) {
+                        String data = mOriginalValues.get(i).name;
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            FilteredArrList.add(new Profile(mOriginalValues.get(i).name, mOriginalValues.get(i).job, mOriginalValues.get(i).email, mOriginalValues.get(i).image));
+                        }
+                    }
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+                }
+                return results;
+            }
+        };
+    }
+}
