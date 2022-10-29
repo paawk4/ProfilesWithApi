b3ccca225a378d519f9736187dadb8d20179df75
ЧетвериковПВ <pavel.chet@mail.ru>
2022-10-29 14:45:49 +0300

    sdf

---
+++ app/src/main/java/com/example/profileswithapi/fragments/CreateAndChangeItemFragment.java
@@ -0,0 +1,10 @@
+package com.example.profileswithapi.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class CreateAndChangeItemFragment extends Fragment {
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
